18099468c58f0e0d8f9224e319e52e504ec8d51e
yaGordeev <yaGordeev@MacBook-Air.local>
2020-07-02 12:10:53 +0300

    add build

---
+++ build/precache-manifest.b0fb8f94427126a7bfca54e45c63c746.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe73aab485ff6aa3bb72ef935f3165c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0abacbaf5183c9c6d3c2",
+    "url": "/static/css/main.397aefaa.chunk.css"
+  },
+  {
+    "revision": "2c988cb84db6fdbdd9be",
+    "url": "/static/js/2.f79312c5.chunk.js"
+  },
+  {
+    "revision": "e52a7c7b6d481094f461dddcbab18065",
+    "url": "/static/js/2.f79312c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0abacbaf5183c9c6d3c2",
+    "url": "/static/js/main.0f8f1dff.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5c3e45fd95589a6fafef0b804795ac88",
+    "url": "/static/media/ava.5c3e45fd.jpg"
+  },
+  {
+    "revision": "ef3b0e6981a47b381e6beb06a67f7203",
+    "url": "/static/media/hadassahmedical.ef3b0e69.jpg"
+  },
+  {
+    "revision": "48d22afa33f6d9d029de51a304d72fc0",
+    "url": "/static/media/logo.48d22afa.png"
+  },
+  {
+    "revision": "8744fd07ab05723e2e14bdfb6dc0a9ae",
+    "url": "/static/media/logo2.8744fd07.png"
+  },
+  {
+    "revision": "a7dfd0ad15b1e931440a64cb4358ff80",
+    "url": "/static/media/member0.a7dfd0ad.jpg"
+  },
+  {
+    "revision": "63377c9d7ed7221af0cf1dae12d948cd",
+    "url": "/static/media/member1.63377c9d.jpg"
+  },
+  {
+    "revision": "a54ef425f83e62cc88c2f9d66653793d",
+    "url": "/static/media/member2.a54ef425.jpg"
+  },
+  {
+    "revision": "d7a2d50fe7062b251e5b172ab55beb62",
+    "url": "/static/media/member3.d7a2d50f.jpg"
+  }
+]);
